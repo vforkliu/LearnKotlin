bb088f222517b4c9bccaeca239489edc2f77c534
liujun <vforkliu@gmail.com>
2023-05-25 09:38:57 +0800

    example coroutines

--- samplecoroutines/src/main/kotlin/Main.kt
+++ samplecoroutines/src/main/kotlin/Main.kt
@@ -1,4 +1,30 @@
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.channels.produce
+
+fun testProduceActor() {
+    runBlocking {
+        val receiveChannel: ReceiveChannel<Int> = produce {
+            var i = 0
+            while(true){
+                delay(1000)
+                send(i)
+                i++
+            }
+        }
+        delay(3000)
+        receiveChannel.cancel()
+        var sendChannel: SendChannel<Int> = actor<Int> {
+            for (ele in channel)
+                ele
+        }
+
+
+    }
+
+}
 fun main(args: Array<String>) {
     GlobalScope.launch {
         println("Code run in coroutine scope")
